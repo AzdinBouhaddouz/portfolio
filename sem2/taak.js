f46b6940f1ca52087c405479aa56c67e07dd41f1
AzdinBouhaddouz <azdin.bouhaddouz@student.ehb.be>
2026-04-02 21:57:17 +0200

    zon, wolk, vogel en zee function gezet

--- sem2/taak.js
+++ sem2/taak.js
@@ -66,3 +66,55 @@ function draw() {
 
 
 
+function tekenLucht() {
+  
+  for (let y = 0; y < height / 2; y++) {
+    let tussen = map(y, 0, height / 2, 0, 1);
+    let kleur = lerpColor(color(255, 150, 50), color(255, 100, 150), tussen);
+    stroke(kleur);
+    line(0, y, width, y);
+  }
+}
+
+function tekenZon(x, y) {
+  
+  noStroke();
+  for (let i = 100; i > 0; i -= 10) {
+    fill(255, 200, 50, map(i, 0, 100, 100, 0));
+    ellipse(x, y, i * 2);
+  }
+  
+  fill(255, 220, 100);
+  ellipse(x, y, 80);
+}
+
+function tekenWolk(x, y) {
+  noStroke();
+  fill(255, 255, 255, 150);
+  ellipse(x, y, 60, 40);
+  ellipse(x + 30, y, 50, 35);
+  ellipse(x - 25, y + 5, 40, 30);
+}
+
+function tekenVogel(x, y) {
+  stroke(50);
+  strokeWeight(2);
+  noFill();
+  
+  let flapOffset = sin(frameCount * 0.2 + x) * 5;
+  beginShape();
+  vertex(x - 10, y + flapOffset);
+  vertex(x, y);
+  vertex(x + 10, y + flapOffset);
+  endShape();
+  strokeWeight(1);
+}
+
+function tekenZee() {
+  
+  noStroke();
+  fill(30, 100, 150);
+  rect(0, height / 2, width, height / 2 - 100);
+  
+  
+  
